d6c2a82323650ed22a2a844e50ce267c09d36880
Adam Jones <admajonse@gmail.com>
2020-02-02 13:36:36 -0500

    add current interface

--- money/src/dollar.adb
+++ money/src/dollar.adb
@@ -6,28 +6,38 @@ package body Dollar is
    --
    function Make 
      (Amount : in Integer)
-      return Object is
-   begin
-      return Object'(Amount => Amount);
-   end Make;
+      return Object is 
+     (Object'(Amount => Amount));
    
    -----------------------------------------------------------------------------
    --
    function Image 
      (This : in Object)
       return String is
-   begin
-      return "$" & Ada.Strings.Fixed.Trim (This.Amount'Img, Ada.Strings.Both);
-   end Image;
+     ("$" & Ada.Strings.Fixed.Trim (This.Amount'Img, Ada.Strings.Both));
+   
+   -----------------------------------------------------------------------------
+   --
+   function Equals 
+     (Left  : in Object;
+      Right : in Object)
+      return Boolean is 
+     (Left.Amount = Right.Amount);
+   
+   -----------------------------------------------------------------------------
+   --
+   function Times 
+     (This       : in Object;
+      Multiplier : in Object)
+      return Object is
+      (Object'(Amount => This.Amount * Multiplier.Amount));
    
    -----------------------------------------------------------------------------
    --
    function Times 
      (This       : in Object;
       Multiplier : in Integer)
-      return Object is
-   begin
-      return Object'(Amount => This.Amount * Multiplier);
-   end Times;
+      return Object is 
+     (Object'(Amount => This.Amount * Multiplier));
 
 end Dollar;
--- money/test/dollar-object_test_data-object_tests.ads
+++ money/test/dollar-object_test_data-object_tests.ads
@@ -9,14 +9,29 @@ package Dollar.Object_Test_Data.Object_Tests is
    type Test_Object is new
      GNATtest_Generated.GNATtest_Standard.Dollar.Object_Test_Data.Test_Object with null record;
 
-   procedure Test_Make_890bb4 (Gnattest_T : in out Test_Object);
-   --  dollar.ads:11:4:Make
+   procedure Test_Make_de4abd (Gnattest_T : in out Test_Object);
+   --  dollar.ads:14:4:Make
 
-   procedure Test_Image_dca781 (Gnattest_T : in out Test_Object);
-   --  dollar.ads:21:4:Image
+   procedure Test_Image_81b81d (Gnattest_T : in out Test_Object);
+   --  dollar.ads:24:4:Image
 
-   procedure Test_Times_d80507 (Gnattest_T : in out Test_Object);
-   --  dollar.ads:32:4:Times
+   procedure Test_Equals_f29847 (Gnattest_T : in out Test_Object);
+   --  dollar.ads:37:4:Equals
+
+   procedure Test_Equal_3bce4c (Gnattest_T : in out Test_Object);
+   --  dollar.ads:51:4:Equal
+
+   procedure Test_Times_c56911 (Gnattest_T : in out Test_Object);
+   --  dollar.ads:65:4:Times
+
+   procedure Test_Multiply_21f420 (Gnattest_T : in out Test_Object);
+   --  dollar.ads:78:4:Multiply
+
+   procedure Test_Times_42bd3e (Gnattest_T : in out Test_Object);
+   --  dollar.ads:92:4:Times
+
+   procedure Test_Multiply_9e944b (Gnattest_T : in out Test_Object);
+   --  dollar.ads:105:4:Multiply
 
 end Dollar.Object_Test_Data.Object_Tests;
 --  end read only
